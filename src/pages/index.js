705e5b5c1d3d0133d7eaebcfa62da86a4de9cfff
Dovtutis <dovtutis@gmail.com>
2021-11-13 22:37:13 +0200

    newsletter and post page added

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,44 @@ import Layout from '../components/Layout';
 import Posts from '../components/Posts';
 import { graphql } from 'gatsby';
 import SEO from '../components/SEO';
-// ...GatsbyImageSharpFluid
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const {
+    allMdx: { nodes: posts },
+  } = data;
+
   return (
     <Layout>
       <Hero showPerson={true} />
+      <Posts posts={posts} title="recently published" />
     </Layout>
   );
 };
+
+export const query = graphql`
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }, limit: 3) {
+      nodes {
+        excerpt
+        id
+        frontmatter {
+          title
+          author
+          date(formatString: "MMMM Do,YYYY")
+          category
+          slug
+          readTime
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage;
